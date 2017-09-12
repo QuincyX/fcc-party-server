11763ba7dae8fe48f0cf8d5609bb7614f299e38d
Quincy <jayever@163.com>
2017-09-12 22:42:16 +0800

    edit index page by nunjucks

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,7 +1,14 @@
 const Base = require('./base.js');
 
 module.exports = class extends Base {
-  indexAction() {
+  async indexAction() {
+    let User = this.mongoose('user')
+    let Post = this.mongoose('post')
+    let a = await User.where({ }).count()
+    this.assign({
+      userNumber: await User.where({ }).count(), 
+      postNumber: await Post.where({ }).count()
+    }); 
     return this.display();
   }
 };
